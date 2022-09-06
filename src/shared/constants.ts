c1304fa0fdc0b97080ab3be9c2d152e350a0f028
Nicholas <nicholas@lanars.com>
2022-09-06 16:11:16 +0300

    Add enum for constant obj

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,7 +1,7 @@
-export const GRADIENT_TYPES = {
-  LINEAR: 'linear',
-  RADIAL: 'radial',
-};
+export enum GRADIENT_TYPES {
+  LINEAR = 'linear',
+  RADIAL = 'radial',
+}
 
 export const maxColorsCount = 9;
 export const defaultGradient = 'linear-gradient(180deg, rgba(242, 178, 56, 1) 0%, rgba(240, 103, 25, 1) 100%)';
