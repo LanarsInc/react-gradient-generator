85ee990fba2353c615afa31b51d3f266885b891f
NicholasZhmakinLanars <nicholas@lanars.com>
2023-05-18 11:44:45 +0300

    MAR-14 Add useOutsideClick custom hook

---
+++ src/shared/hooks/useOutsideClick.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from 'react';
+
+export const useOutsideClick = (
+  callback: () => void,
+  excludedElement?: HTMLDivElement | null
+) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (excludedElement && event.target === excludedElement) {
+        return;
+      }
+
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [callback, excludedElement]);
+
+  return ref;
+};
