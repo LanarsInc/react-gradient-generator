7e9bb2ff293be60ab32623cfe72c13054f592387
Maryna Zubkova <maryna.zubkova@lanars.com>
2023-10-06 17:50:23 +0300

    Update rules in eslint, tsconfig

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,5 +30,7 @@ module.exports = {
     'jsx-a11y/no-static-element-interactions': 0,
     'jsx-a11y/click-events-have-key-events': 0,
     'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off',
   },
 };
